6bbc110d4bb23a8398eaa7e47055267034d45aa5
neffoxxx <artemkampod4@gmail.com>
2022-05-06 14:49:35 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var auth1 = document.getElementById("auth1");
+var auth2 = document.getElementById("auth2");
+var auth3 = document.getElementById("auth3");
+var auth4 = document.getElementById("auth4");
+var auth5 = document.getElementById("auth5");
+auth1.onclick = function() {
+    confirm('Де немає свободи, там немає і вітчизни.');
+}
+auth2.onclick = function() {
+    confirm('В своїй хаті своя й правда, І сила, і воля.')
+}
+auth3.onclick = function() {
+    confirm('Вітчизна — ось і альфа, і омега!')
+}
+auth4.onclick = function() {
+    confirm('Нації вмирають не від інфаркту. Спочатку їм відбирає мову.')
+}
+auth5.onclick = function() {
+    confirm('Ще не вмерла України і слава, і воля.')
+}
+
